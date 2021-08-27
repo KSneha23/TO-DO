99472b52835ee93d57a36a8a74f9a93002326c5a
K_Sneha <snehajyothi2302@gmail.com>
2021-08-27 17:00:46 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-sneha:test123@cluster0.q2lcn.mongodb.net/todolistDB",{useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb+srv://admin-sneha:test123@cluster0.q2lcn.mongodb.net/todolistDB",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemSchema = {
   name:String
